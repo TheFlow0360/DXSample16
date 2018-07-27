d100655fdab97873b578454cf24e9123e7d6bbb1
Florian Koch <floriankochde@arcor.de>
2018-07-27 09:20:04 +0200

    created sample project

---
+++ DXSample16/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using DevExpress.Mvvm.UI;
+using DevExpress.Xpf.Core;
+
+namespace DXSample16
+{
+    public partial class MainWindow : ThemedWindow
+    {
+        private NotificationService NotificationService { get; set; }
+
+        private ImageSource Icon { get; set; }
+
+        private Random Rng { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitNotificationService();
+            Rng = new Random();
+            Icon = new BitmapImage(new Uri("pack://application:,,,/DXSample16;component/dummy.png"));
+        }
+
+        private void InitNotificationService()
+        {
+            var appId = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false).Cast<AssemblyTitleAttribute>().First().Title;
+            var notificationTemplate = Application.Current.Resources["CustomNotificationTemplate"] as DataTemplate;
+            var notificationStyle = Application.Current.Resources["CustomNotificationStyle"] as Style;
+
+            NotificationService = new NotificationService
+            {
+                ApplicationId = appId,
+                UseWin8NotificationsIfAvailable = false,
+                PredefinedNotificationTemplate = NotificationTemplate.ShortHeaderAndTwoTextFields,
+                CustomNotificationPosition = NotificationPosition.BottomRight,
+                CustomNotificationTemplate = notificationTemplate,
+                CustomNotificationStyle = notificationStyle
+            };
+        }
+
+        private void NoButtons_OnClick(Object sender, RoutedEventArgs e)
+        {
+            var viewModel = new NotificationViewModel("Notification!", "Additional Text", "Another line", Icon, null);
+            ShowNotification(viewModel);
+        }
+
+        private void WithButtons_OnClick(Object sender, RoutedEventArgs e)
+        {
+            var buttons = new List<NotificationViewModel.ButtonInfo>();
+            var count = Rng.Next(3, 15);
+            for (var i = 0; i < count; i++)
+            {
+                var iCopy = i;
+                buttons.Add(new NotificationViewModel.ButtonInfo($"Item {i}", () => Output.Text = $"Item {iCopy}"));
+            }
+            var viewModel = new NotificationViewModel("Notification!", "Additional Text", null, Icon, buttons);
+            ShowNotification(viewModel);
+        }
+
+        private void ShowNotification(NotificationViewModel viewModel)
+        {
+            var notification = NotificationService.CreateCustomNotification(viewModel);
+            viewModel.CloseSelf = notification.Hide;
+            notification.ShowAsync();
+        }
+    }
+}
---
+++ DXSample16/NotificationViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Media;
+
+namespace DXSample16
+{
+    public class NotificationViewModel
+    {
+        public ImageSource Image { get; }
+
+        public String Caption { get; }
+
+        public String TextZeile1 { get; }
+
+        public String TextZeile2 { get; }
+
+        public Boolean ShowList => Items?.Count > 0;
+
+        public IList<ButtonInfo> Items { get; }
+
+        public Int32 Height => ShowList ? 200 : 89;             // this isn't a good solution
+
+        public ButtonInfo SelectedItem
+        {
+            get => null;
+            set
+            {
+                value?.Execute();
+                CloseSelf?.Invoke();
+            }
+        }
+
+        public Action CloseSelf { get; set; }
+
+        public NotificationViewModel(String caption, String textZeile1, String textZeile2, ImageSource image, IList<ButtonInfo> buttons = null)
+        {
+            Image = image;
+            Caption = caption;
+            TextZeile1 = textZeile1;
+            TextZeile2 = textZeile2;
+
+            Items = buttons;
+        }
+
+        public class ButtonInfo
+        {
+            public String Caption { get; }
+
+            public Action Execute { get; }
+
+            public ButtonInfo(String caption, Action execute)
+            {
+                Caption = caption;
+                Execute = execute;
+            }
+        }
+    }
+}
